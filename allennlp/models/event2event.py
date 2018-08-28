5d40734087d7aaa7931f5d68d47e90ed34747bca
Maarten Sap <msap@skvl.cs.uw.edu>
2018-08-28 14:09:58 -0700

    tweaks

--- allennlp/models/event2event.py
+++ allennlp/models/event2event.py
@@ -45,9 +45,9 @@ class StateDecoderEarlyFusion:
         
         self._recalls = {}
         for n in event2event.dim_names:
-            self._recalls[n] = UnigramRecall()
+            # self._recalls[n] = UnigramRecall()
             # self._recalls[n] = BleuN(n=2)
-            # self._recalls[n] = RougeN(N=2)
+            self._recalls[n] = RougeL()
 
 
 @Model.register("event2event")
